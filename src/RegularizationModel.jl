9c356288983c6051b7b442bf97be1ee905e1a985
dpo <dpo@users.noreply.github.com>
2021-04-23 13:47:28 +0000

    :robot: Format .jl files

--- src/RegularizationModel.jl
+++ src/RegularizationModel.jl
@@ -12,58 +12,72 @@ Implements
     f(x) + ½ ρ ‖x - z‖²
 """
 mutable struct RegNLP <: AbstractNLPModel
-  meta :: NLPModelMeta
-  inner :: AbstractNLPModel
+  meta::NLPModelMeta
+  inner::AbstractNLPModel
   ρ
   z
 end
 
-show_header(io :: IO, nlp :: RegNLP) = println(io, "RegNLP - Regularized model")
+show_header(io::IO, nlp::RegNLP) = println(io, "RegNLP - Regularized model")
 
-function Base.show(io :: IO, nlp :: RegNLP)
+function Base.show(io::IO, nlp::RegNLP)
   show_header(io, nlp)
   show(io, nlp.meta)
   show(io, nlp.inner.counters)
 end
 
-function RegNLP(nlp :: AbstractNLPModel, ρ, z)
+function RegNLP(nlp::AbstractNLPModel, ρ, z)
   n = nlp.meta.nvar
   nnzh = nlp.meta.nnzh + n
-  return RegNLP(NLPModelMeta(n, x0=nlp.meta.x0, nnzh=nnzh),
-                nlp, ρ, z)
+  return RegNLP(NLPModelMeta(n, x0 = nlp.meta.x0, nnzh = nnzh), nlp, ρ, z)
 end
 
 @default_counters RegNLP inner
 
-function NLPModels.obj(nlp :: RegNLP, x :: AbstractVector)
+function NLPModels.obj(nlp::RegNLP, x::AbstractVector)
   return obj(nlp.inner, x) + nlp.ρ * norm(x - nlp.z)^2 / 2
 end
 
-function NLPModels.grad!(nlp :: RegNLP, x :: AbstractVector, g :: AbstractVector)
+function NLPModels.grad!(nlp::RegNLP, x::AbstractVector, g::AbstractVector)
   grad!(nlp.inner, x, g)
   g .+= nlp.ρ * (x - nlp.z)
   return g
 end
 
-function NLPModels.hess_structure!(nlp :: RegNLP, rows :: AbstractVector{<: Integer}, cols :: AbstractVector{<: Integer})
+function NLPModels.hess_structure!(
+  nlp::RegNLP,
+  rows::AbstractVector{<:Integer},
+  cols::AbstractVector{<:Integer},
+)
   nz = nlp.inner.meta.nnzh
   n = nlp.meta.nvar
   @views hess_structure!(nlp.inner, rows[1:nz], cols[1:nz])
-  rows[nz+1:end] .= 1:n
-  cols[nz+1:end] .= 1:n
+  rows[(nz + 1):end] .= 1:n
+  cols[(nz + 1):end] .= 1:n
   return rows, cols
 end
 
-function NLPModels.hess_coord!(nlp :: RegNLP, x :: AbstractVector, vals :: AbstractVector; obj_weight :: Real=1.0)
+function NLPModels.hess_coord!(
+  nlp::RegNLP,
+  x::AbstractVector,
+  vals::AbstractVector;
+  obj_weight::Real = 1.0,
+)
   nz = nlp.inner.meta.nnzh
   n = nlp.meta.nvar
-  @views hess_coord!(nlp.inner, x, vals[1:nz]; obj_weight=obj_weight)
-  vals[nz+1:end] .= nlp.ρ * obj_weight
+  @views hess_coord!(nlp.inner, x, vals[1:nz]; obj_weight = obj_weight)
+  vals[(nz + 1):end] .= nlp.ρ * obj_weight
   return vals
 end
 
-function NLPModels.hprod!(nlp :: RegNLP, x :: AbstractVector, v :: AbstractVector, Hv :: AbstractVector; obj_weight :: Real=1.0)
-  hprod!(nlp.inner, x, v, Hv, obj_weight=obj_weight)
+function NLPModels.hprod!(
+  nlp::RegNLP,
+  x::AbstractVector,
+  v::AbstractVector,
+  Hv::AbstractVector;
+  obj_weight::Real = 1.0,
+)
+  hprod!(nlp.inner, x, v, Hv, obj_weight = obj_weight)
   Hv .+= nlp.ρ * obj_weight * v
   return Hv
 end
